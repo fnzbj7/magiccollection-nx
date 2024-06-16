ceaf16b4a1c96ff5d13b4000e9e0f2d337fde1cd
fnzbj7 <jukijun91@gmail.com>
2024-06-16 23:35:28 +0200

    ADD information after upload card

--- apps/magiccollection/src/app/magic/modify-card/dto/card-quantity.model.ts
+++ apps/magiccollection/src/app/magic/modify-card/dto/card-quantity.model.ts
@@ -5,3 +5,13 @@ export class CardQuantity {
     language!: string;
     type!: string;
 }
+
+export interface PriorityList {
+    cardNumber: string;
+    upload: number;
+    was: number;
+    have: number;
+    uploadF: number;
+    wasF: number;
+    haveF: number;
+}
--- apps/magiccollection/src/app/magic/modify-card/modify-card.component.ts
+++ apps/magiccollection/src/app/magic/modify-card/modify-card.component.ts
@@ -8,8 +8,9 @@ import { ModifyQtyEnum } from '../../model/modify-qty.enum';
 import { Card, CardLayout, CardVariationDto } from '../../model/card.model';
 import { CardWithFoil } from './dto/foil.dto';
 import { AfterFinishForm } from './modify-form/model/after-finish-form.model';
-import { CardQuantity } from './dto/card-quantity.model';
+import { CardQuantity, PriorityList } from './dto/card-quantity.model';
 import { AuthenticationService } from '../../auth/authentication.service';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
 enum PageStep {
     FORM = 'from',
@@ -45,6 +46,10 @@ export class ModifyCardComponent implements OnInit, OnDestroy {
     rawCardNumbers!: CardWithFoil[];
 
     mode!: ModifyMode;
+
+    CardLayout = CardLayout;
+    faAngleDoubleRight = faAngleDoubleRight;
+    filteredNewCards: PriorityList[] | null = [];
 
     constructor(
         private modifyCardService: ModifyCardService,
@@ -94,14 +99,16 @@ export class ModifyCardComponent implements OnInit, OnDestroy {
                 this.isNewCardsFinished = true;
                 this.isNewCardsLoading = false;
                 // Compare to the uploaded cards
+                this.calculateWholeNewCards(cards);
+                /* --------- Other ------------ */
                 const cardQuantitys = this.reducedArr.cardQuantitys;
-                const filteredNewCards = cardQuantitys.filter(x => {
+                this.filteredNewCards = cardQuantitys.map(x => {
                     const foundCard = cards.find(
                         card => card.cardNumber && +card.cardNumber === x.cardNumber,
                     );
 
                     if (!foundCard) {
-                        return !false;
+                        return {} as PriorityList;
                     }
 
                     let language = 'En'; // TODO maybe tunnel language from a different place
@@ -112,75 +119,107 @@ export class ModifyCardComponent implements OnInit, OnDestroy {
                     ) {
                         language = this.reducedArr.cardQuantitys[0].language;
                     }
-                    const priorityList = [];
+
                     if (foundCard.cardVariation) {
                         // Language needed
-                        priorityList.push({
-                            upload: x.cardQuantityFoil,
-                            have: foundCard.cardVariation[
-                                ('f' + language) as keyof CardVariationDto
-                            ],
-                        });
-                        priorityList.push({
+                        const have =
+                            foundCard.cardVariation[('n' + language) as keyof CardVariationDto];
+                        const haveF =
+                            foundCard.cardVariation[('f' + language) as keyof CardVariationDto];
+                        return {
+                            cardNumber: ('' + x.cardNumber).padStart(3, '0'),
                             upload: x.cardQuantity,
-                            have: foundCard.cardVariation[
-                                ('n' + language) as keyof CardVariationDto
-                            ],
-                        });
-                    } else {
-                        priorityList.push({
-                            upload: x.cardQuantityFoil,
-                            have: foundCard.cardAmountFoil,
-                        });
-                        priorityList.push({ upload: x.cardQuantity, have: foundCard.cardAmount });
+                            was: have - x.cardQuantity,
+                            have,
+                            uploadF: x.cardQuantityFoil,
+                            wasF: haveF - x.cardQuantityFoil,
+                            haveF,
+                        };
                     }
-
-                    for (const priority of priorityList) {
-                        // if upload and have both 0, then skip
-                        if (priority.upload < priority.have) {
-                            return false;
-                        }
-                        if (priority.upload > 0 && priority.upload === priority.have) {
-                            return true;
-                        }
-                    }
-                    return false;
+                    return {} as PriorityList;
                 });
-
-                const reducedCards = this.rawCardNumbers.reduce(
-                    (reduceValue: CardQuantity[], currValue: CardWithFoil) => {
-                        const isProcessed = reduceValue.find(
-                            f => f.cardNumber === currValue.cardNum,
-                        );
-                        const found = filteredNewCards.find(
-                            f => f.cardNumber === currValue.cardNum,
-                        );
-                        if (isProcessed || !found) {
-                            return reduceValue;
-                        } else {
-                            if (currValue.isFoil) {
-                                reduceValue.push(found);
-                            } else if (!currValue.isFoil && found.cardQuantityFoil === 0) {
-                                reduceValue.push(found);
-                            }
-                        }
-
-                        return reduceValue;
-                    },
-                    [],
-                );
-
-                // Creating an array from the new cards
-                this.newCards = reducedCards.map(x => {
-                    const card = new Card();
-                    card.cardAmount = x.cardQuantity;
-                    card.cardAmountFoil = x.cardQuantityFoil;
-                    card.cardExpansion = this.cardSet;
-                    card.cardNumber = ('' + x.cardNumber).padStart(3, '0');
-                    card.layout = CardLayout.NORMAL;
-                    return card;
-                });
+                // .map(qt => ({...qt, cardNumber: (''+qt.cardNumber).padStart(3, '0')}));
             });
+    }
+    calculateWholeNewCards(cards: Card[]) {
+        const cardQuantitys = this.reducedArr.cardQuantitys;
+        const filteredNewCards = cardQuantitys.filter(x => {
+            const foundCard = cards.find(
+                card => card.cardNumber && +card.cardNumber === x.cardNumber,
+            );
+
+            if (!foundCard) {
+                return !false;
+            }
+
+            let language = 'En'; // TODO maybe tunnel language from a different place
+            if (
+                this.reducedArr &&
+                this.reducedArr.cardQuantitys &&
+                this.reducedArr.cardQuantitys[0]
+            ) {
+                language = this.reducedArr.cardQuantitys[0].language;
+            }
+            const priorityList = [];
+            if (foundCard.cardVariation) {
+                // Language needed
+                priorityList.push({
+                    upload: x.cardQuantityFoil,
+                    have: foundCard.cardVariation[('f' + language) as keyof CardVariationDto],
+                });
+                priorityList.push({
+                    upload: x.cardQuantity,
+                    have: foundCard.cardVariation[('n' + language) as keyof CardVariationDto],
+                });
+            } else {
+                priorityList.push({
+                    upload: x.cardQuantityFoil,
+                    have: foundCard.cardAmountFoil,
+                });
+                priorityList.push({ upload: x.cardQuantity, have: foundCard.cardAmount });
+            }
+
+            for (const priority of priorityList) {
+                // if upload and have both 0, then skip
+                if (priority.upload < priority.have) {
+                    return false;
+                }
+                if (priority.upload > 0 && priority.upload === priority.have) {
+                    return true;
+                }
+            }
+            return false;
+        });
+
+        const reducedCards = this.rawCardNumbers.reduce(
+            (reduceValue: CardQuantity[], currValue: CardWithFoil) => {
+                const isProcessed = reduceValue.find(f => f.cardNumber === currValue.cardNum);
+                const found = filteredNewCards.find(f => f.cardNumber === currValue.cardNum);
+                if (isProcessed || !found) {
+                    return reduceValue;
+                } else {
+                    if (currValue.isFoil) {
+                        reduceValue.push(found);
+                    } else if (!currValue.isFoil && found.cardQuantityFoil === 0) {
+                        reduceValue.push(found);
+                    }
+                }
+
+                return reduceValue;
+            },
+            [],
+        );
+
+        // Creating an array from the new cards
+        this.newCards = reducedCards.map(x => {
+            const card = new Card();
+            card.cardAmount = x.cardQuantity;
+            card.cardAmountFoil = x.cardQuantityFoil;
+            card.cardExpansion = this.cardSet;
+            card.cardNumber = ('' + x.cardNumber).padStart(3, '0');
+            card.layout = CardLayout.NORMAL;
+            return card;
+        });
     }
 
     resetPage() {
@@ -189,6 +228,7 @@ export class ModifyCardComponent implements OnInit, OnDestroy {
         this.isNewCardsLoading = false;
         this.isNewCardsFinished = false;
         this.newCards = null;
+        this.filteredNewCards = null;
     }
 
     ngOnDestroy() {
