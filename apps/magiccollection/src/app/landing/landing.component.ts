45816016b87cf5cb502113520693252be3bead6b
fnzbj7 <jukijun91@gmail.com>
2026-02-14 18:33:39 +0100

    Landing page navigation adjusted

--- apps/magiccollection/src/app/landing/landing.component.ts
+++ apps/magiccollection/src/app/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthenticationService } from '../auth/authentication.service';
+import { magicSetArray } from '@magiccollection/magic/magic-card-list/magic-cards-list.service';
 
 @Component({
     selector: 'app-landing',
@@ -10,6 +11,7 @@ import { AuthenticationService } from '../auth/authentication.service';
 export class LandingComponent implements OnInit, OnDestroy {
     isLoggedIn = false;
     private currentUserSub?: Subscription;
+    checkCardsMenu = ['/', 'cards', 'list'];
 
     constructor(private authenticationService: AuthenticationService) {}
 
@@ -17,6 +19,18 @@ export class LandingComponent implements OnInit, OnDestroy {
         this.isLoggedIn = this.authenticationService.currentUserValue !== null;
         this.currentUserSub = this.authenticationService.currentUserSubject.subscribe(user => {
             this.isLoggedIn = user !== null;
+            if (this.isLoggedIn && user?.id) {
+                this.checkCardsMenu = [
+                    '/',
+                    'cards',
+                    'list',
+                    'user',
+                    '' + user.id,
+                    magicSetArray[0].name,
+                ];
+            } else {
+                this.checkCardsMenu = ['/', 'cards', 'list'];
+            }
         });
     }
 
